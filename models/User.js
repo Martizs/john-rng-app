64c2136d5d4493b89c0c247ffb5d72c7c814cb06
Martynas <martynas@sdhouse.lt>
2022-05-14 21:05:06 +0300

    db connection setup

---
+++ models/User.js
@@ -0,0 +1,17 @@
+// models/User.js
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    password: { type: String, required: true, select: false },
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
