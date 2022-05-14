dcdeb989c44f57c9c555ec829c1276468c472ce9
Fus <martynas@zimmermanzimmerman.nl>
2022-05-14 20:09:24 +0300

    final touches before start (#2)
    Co-authored-by: Martynas <martynas@sdhouse.lt>

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+    console.log('api/hello has been called');
+
+    res.statusCode = 200;
+    res.json({ name: 'John Doe' });
+};
