ff9116013af1c62ad09929c1b0ac409fa98355af
Martynas <martynas@sdhouse.lt>
2022-08-21 13:28:25 +0300

    Some restructuring

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import Layout from 'components/layout';
+import Layout from 'components/Layout';
 import { AuthContext } from 'hooks/authContext';
 import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
