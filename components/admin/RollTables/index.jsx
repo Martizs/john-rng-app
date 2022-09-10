09864b636fa2620fc8f93cbb62a20328497c3819
Martynas <martynas@sdhouse.lt>
2022-09-10 13:39:19 +0300

    Modal generalization

--- components/admin/RollTables/index.jsx
+++ components/admin/RollTables/index.jsx
@@ -1,9 +1,36 @@
+import { Button } from 'components/Button';
+import { InputField } from 'components/InputField';
+import { Modal } from 'components/Modal';
+import { useRef, useState } from 'react';
 import styles from './RollTables.module.css';
 
 export const RollTables = () => {
+    const [createTable, setCreatetable] = useState(false);
+    const createTitle = useRef();
+
+    const onCreate = () => {
+        console.log('title', createTitle.current);
+    };
+
+    const hangleCreateClose = () => {
+        createTitle.current = undefined;
+        setCreatetable(false);
+    };
+
     return (
         <div className={styles.container}>
-            <div>Roll tables divvv</div>
+            <Button
+                title="Add table"
+                onClick={() => setCreatetable(true)}
+                type="success"
+            />
+            <Modal isOpen={createTable} onRequestClose={hangleCreateClose}>
+                <InputField
+                    placeholder="Title"
+                    onChange={(e) => (createTitle.current = e.target.value)}
+                />
+                <Button title="Add" type="success" onClick={onCreate} />
+            </Modal>
         </div>
     );
 };
