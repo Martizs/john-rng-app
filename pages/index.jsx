5953bcb26f8b5e778841ab55c5a50553642a111d
Martynas <martynas@sdhouse.lt>
2022-10-07 17:46:26 +0300

    Treasure chest open close done

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,26 +1,59 @@
 import axios from 'axios';
 import { ItemCard } from 'components/ItemCard';
 import { showError } from 'lib/ui/utils';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+    const [openChest, setOpenChest] = useState(false);
     const [treasureItems, setTreasureItems] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get('/api/treasureItems')
-            .then((resp) => {
-                setTreasureItems(resp.data);
-            })
-            .catch(showError);
-    }, []);
-
-    // eslint-disable-next-line no-console
-    console.log('treasureItems', treasureItems);
+    const handleToggleChest = () => {
+        if (!openChest) {
+            axios
+                .get('/api/treasureItems')
+                .then((resp) => {
+                    setOpenChest(true);
+                    setTreasureItems(resp.data);
+                })
+                .catch(showError);
+        } else {
+            setOpenChest(false);
+            setTreasureItems([]);
+        }
+    };
 
     return (
         <div className={styles.container}>
+            {openChest ? (
+                <a
+                    href="https://pngtree.com/freepng/an-open-treasure-chest-illustration_4619909.html?sol=downref&id=bef"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.attributionLink}
+                >
+                    treasure chest clipart PNG Designed By 588ku
+                </a>
+            ) : (
+                <a
+                    href="https://pngtree.com/freepng/cartoon-brown-treasure-chest-illustration_4617990.html?sol=downref&id=bef"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.attributionLink}
+                >
+                    treasure chest clipart PNG Designed By 588ku
+                </a>
+            )}
+            <img
+                onClick={handleToggleChest}
+                className={styles.chestButton}
+                src={
+                    openChest
+                        ? '/images/openChest.png'
+                        : '/images/closedChest.png'
+                }
+            />
+
             <div className={styles.mainContainer}>
                 {treasureItems.map((treasureItem) => (
                     <ItemCard
